1ed7d8f57fc86fe1ba9d503e6bf7a9f4bee71ac7
as2745 <60445218+as2745@users.noreply.github.com>
2020-06-14 15:54:06 -0400

    Add files via upload

--- public_html/RegLoginCSS/login.php
+++ public_html/RegLoginCSS/login.php
@@ -42,6 +42,14 @@ if(isset($_POST["login"])){
 						$rpassword = $result["password"];
 						if(password_verify($password, $rpassword)){
 							echo "<div>Passwords matched! You are technically logged in!</div>";
+							$_SESSION["user"] = array(
+								"id"=>$result["id"],
+								"email"=>$result["email"],
+								"first_name"=>$result["first_name"],
+								"last_name"=>$result["last_name"]
+							);
+							echo var_export($_SESSION, true);
+							header("Location: home.php");
 						}
 						else{
 							echo "<div>Invalid password!</div>";
