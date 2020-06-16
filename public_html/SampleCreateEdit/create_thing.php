67de2a3eb1da897920c4416d5bd8240645abcb8b
as2745 <60445218+as2745@users.noreply.github.com>
2020-06-15 22:26:17 -0400

    Add files via upload

--- public_html/SampleCreateEdit/create_thing.php
+++ public_html/SampleCreateEdit/create_thing.php
@@ -5,6 +5,12 @@
 	<label for="accnumber">Account Number
 	<input type="number" id="AccNum" name="Account_Number" />
 	</label>
+	<label for="acctype">Account Type
+	<input type="text" id="AccTyp" name="Account_Type" />
+	</label>
+		<label for="balance">Balance
+	<input type="number" id="balance" name="Balance" />
+	</label>
 	<input type="submit" name="Bank" value="Create Account"/>
 </form>
 
@@ -12,15 +18,19 @@
 if(isset($_POST["created"])){
     $name = $_POST["Name"];
     $Accnum = $_POST["Account_Number"];
-    if(!empty($name) && !empty($Accnum)){
+	$Acctyp = $_POST["Account_Type"];
+	$balance = $_POST["Balance"];
+    if(!empty($name) && !empty($Accnum)&& !empty($Acctyp)&& !empty($balance)){
         require("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
-            $stmt = $db->prepare("INSERT INTO Bank_Account (Name, Account_Number) VALUES ($name, $Accnum)");
+            $stmt = $db->prepare("INSERT INTO Bank_Account (Name, Account_Number, Account_Type,Balance) VALUES ($name, $Accnum, $Acctyp,$balance)");
             $result = $stmt->execute(array(
                 ":Name" => $name,
                 ":Account_Number" => $Accnum
+				":Account_Type"=> $Acctyp
+				":Balance"=> $balance
             ));
             $e = $stmt->errorInfo();
             if($e[0] != "00000"){
@@ -41,7 +51,7 @@ if(isset($_POST["created"])){
         }
     }
     else{
-        echo "Name and Account_Number must not be empty.";
+        echo "Name, Account Number, Account Type and Balance must not be empty.";
     }
 }
 ?>
