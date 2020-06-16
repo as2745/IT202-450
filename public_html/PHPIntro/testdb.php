5e8538e3294951427d460a181da8363a750dbf1a
as2745 <60445218+as2745@users.noreply.github.com>
2020-06-15 21:20:23 -0400

    Add files via upload

--- public_html/PHPIntro/testdb.php
+++ public_html/PHPIntro/testdb.php
@@ -4,13 +4,13 @@ require("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 try{
 	$db = new PDO($connection_string, $dbuser, $dbpass);
-	$stmt = $db->prepare("CREATE TABLE `Users` (
-				`id` int auto_increment not null,
-				`email` varchar(100) not null unique,
-				`created` timestamp default current_timestamp,
-				`modified` timestamp default current_timestamp on update current_timestamp,
-				PRIMARY KEY (`id`)
-				) CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$stmt = $db->prepare("CREATE TABLE Bank_Account (
+	Name VARCHAR(50),
+	Account Number INT(9),
+	Account type VARCHAR(8)
+	Balance DECIMAL(10,2)
+	PRIMARY KEY(Account Number)
+	) CHARACTER SET utf8 COLLATE utf8_general_ci");
 	$r = $stmt->execute();
 	echo var_export($stmt->errorInfo(), true);
 	echo var_export($r, true);
