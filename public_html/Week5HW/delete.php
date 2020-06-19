43ee2fdd1710e920f1ebdc8990f9af9b23915c10
as2745 <60445218+as2745@users.noreply.github.com>
2020-06-19 16:45:35 -0400

    Add files via upload

--- public_html/Week5HW/delete.php
+++ public_html/Week5HW/delete.php
@@ -17,7 +17,6 @@
 require("common.inc.php");
 $db = getDB();
 $Accnum1 = $_POST["Account_Number"];
-//example usage, change/move as needed
 		try{
 			$stmt = $db->prepare("DELETE from Bank_Account where Account_Number=$Accnum1");
                     $result = $stmt->execute(array(
