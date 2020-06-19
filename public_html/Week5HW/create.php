ba686d3c51420b4a494fa5f39e59e5f2df14ce0e
as2745 <60445218+as2745@users.noreply.github.com>
2020-06-19 15:43:23 -0400

    Add files via upload

--- public_html/Week5HW/create.php
+++ public_html/Week5HW/create.php
@@ -1,18 +1,3 @@
-<form method="POST">
-	<label for="name">Account Name
-	<input type="text" id="Name" name="Name" />
-	</label>
-	<label for="accnumber">Account Number
-	<input type="number" id="AccNum" name="Account_Number" />
-	</label>
-	<label for="acctype">Account Type
-	<input type="text" id="AccTyp" name="Account_Type" />
-	</label>
-	<label for="balance">Balance
-	<input type="number" id="balance" name="Balance" />
-	</label>
-	<input type="submit" name="Bank" value="Create Account"/>
-</form>
 <?php
 require("common.inc.php");
 $db = getDB();
