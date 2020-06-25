d7614c39252fc5f609e6c0dd469baaf6eb0c2618
as2745 <60445218+as2745@users.noreply.github.com>
2020-06-25 16:18:23 -0400

    Add files via upload

--- public_html/BasicTables/CREATE_TABLE_TRANSACTIONS.sql
+++ public_html/BasicTables/CREATE_TABLE_TRANSACTIONS.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Transactions{
+CREATE TABLE Transactions(
     id int auto_increment,
     act_src_id int not null,
     act_dest_id int null,
@@ -8,6 +8,6 @@ CREATE TABLE Transactions{
     expected_total decimal (12,2)
     created datetime default current_timestamp,
     primary key (id),
-    foreign key (act_src_id) references Accounts.id,
-    foreign key (act_dest_id) references Accounts.id
-}
+    foreign key (act_src_id) references Accounts(id),
+    foreign key (act_dest_id) references Accounts(id)
+)
