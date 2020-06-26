6d7e8f0c1dbc994005c37de505a80629646416c0
as2745 <60445218+as2745@users.noreply.github.com>
2020-06-25 21:27:24 -0400

    Add files via upload

--- public_html/Week5HW/delete.php
+++ public_html/Week5HW/delete.php
@@ -1,7 +1,25 @@
+<form method="POST">
+	<label for="name">Account Name
+	<input type="text" id="Name" name="Name" />
+	</label>
+	<label for="accnumber">Account Number
+	<input type="number" id="AccNum" name="Account_Number" />
+	</label>
+	<label for="acctype">Account Type
+	<input type="text" id="AccTyp" name="Account_Type" />
+	</label>
+	<label for="balance">Balance
+	<input type="number" id="balance" name="Balance" />
+	</label>
+	<input type="submit" name="Bank" value="Delete Account"/>
+</form>
 <?php
 require("common.inc.php");
 $db = getDB();
+$Accnum1 = $_POST["Account_Number"];
 //example usage, change/move as needed
-$stmt = $db->prepare("SELECT * FROM Things");
-$stmt->execute();
+$stmt = $db->prepare("DELETE from Bank_Account where Account_Number=$Accnum1");
+                    $result = $stmt->execute(array(
+                        ":id" => $Accnum1
+                    ));
 ?>
