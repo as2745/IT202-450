ca2f14b94b8afe6d9697d0eeb107a47c017af18b
as2745 <60445218+as2745@users.noreply.github.com>
2020-06-25 22:17:27 -0400

    Add files via upload

--- public_html/Week6/search.php
+++ public_html/Week6/search.php
@@ -17,28 +17,16 @@ if(isset($search)) {
             $stmt->execute([":thing"=>$Acctyp]);
             //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			foreach($results as $row){
+				echo '<li>';
+                echo get($row, "name");
+                echo get($row, "quantity");
+                echo '<a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a>';
+            echo '</li>';
+			}
         } catch (Exception $e) {
             echo $e->getMessage();
         }
     }
 }
 ?>
-<!--This part will introduce us to PHP templating,
-note the structure and the ":" -->
-<!-- note how we must close each check we're doing as well-->
-<?php if(isset($results) && count($results) > 0):?>
-    <p>This shows when we have results</p>
-    <ul>
-        <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
-        we're also using our helper function to safely return a value based on our key/column name.-->
-        <?php foreach($results as $row):?>
-            <li>
-                <?php echo get($row, "name")?>
-                <?php echo get($row, "quantity");?>
-                <a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a>
-            </li>
-        <?php endforeach;?>
-    </ul>
-<?php else:?>
-    <p>This shows when we don't have results</p>
-<?php endif;?>
