05cd6dd1c6ddf5487e388b85638d715ce928bec1
as2745 <60445218+as2745@users.noreply.github.com>
2020-06-25 22:33:11 -0400

    Add files via upload

--- public_html/Week6/search.php
+++ public_html/Week6/search.php
@@ -1,33 +1,50 @@
+<?php
+$search = "";
+if(isset($_POST["search"])){
+    $search = $_POST["search"];
+}
+?>
 <form method="POST">
-    <label for="acctype">Account_Type
-	<input type="text" id="AccType" name="Account_Type" />
-	</label>
+    <input type="text" name="search" placeholder="Search for Thing"
+    value="<?php echo $search;?>"/>
     <input type="submit" value="Search"/>
 </form>
 <?php
-//if(isset($search)) {
-	$Acctyp = $_POST["Account_Type"];
-	echo $Acctyp;
+if(isset($search)) {
+
     require("common.inc.php");
+	echo $search;
     $query = file_get_contents(__DIR__ . "/queries/SEARCH_TABLE_THINGS.sql");
-	echo $query;
     if (isset($query) && !empty($query)) {
         try {
             $stmt = getDB()->prepare($query);
+			echo $query;
             //Note: With a LIKE query, we must pass the % during the mapping
-            $stmt->execute([":thing"=>$Acctyp]);
+            $stmt->execute([":thing"=>$search]);
             //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-			foreach($results as $row){
-				echo '<li>';
-                echo get($row, "name");
-                echo get($row, "quantity");
-                echo '<a href="delete.php?thingId=<?php echo get($row, "id");?>Delete</a>';
-            echo '</li>';
-			}
         } catch (Exception $e) {
             echo $e->getMessage();
         }
     }
-//}
+}
 ?>
+<!--This part will introduce us to PHP templating,
+note the structure and the ":" -->
+<!-- note how we must close each check we're doing as well-->
+<?php if(isset($results) && count($results) > 0):?>
+    <p>This shows when we have results</p>
+    <ul>
+        <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
+        we're also using our helper function to safely return a value based on our key/column name.-->
+        <?php foreach($results as $row):?>
+            <li>
+                <?php echo get($row, "name")?>
+                <?php echo get($row, "quantity");?>
+                <a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a>
+            </li>
+        <?php endforeach;?>
+    </ul>
+<?php else:?>
+    <p>This shows when we don't have results</p>
+<?php endif;?>
