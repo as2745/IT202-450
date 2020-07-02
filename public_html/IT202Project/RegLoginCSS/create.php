be23a50fb94e798a16fde295a43396e63e829465
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-02 00:49:35 -0400

    Update create.php

--- public_html/IT202Project/RegLoginCSS/create.php
+++ public_html/IT202Project/RegLoginCSS/create.php
@@ -2,7 +2,7 @@
 include("header.php");
 
 $email=$_SESSION["user"]["email"];
-echo "Hello". $_SESSION["user"]["id"];?>
+echo "Hello". $email;?>
 <form method="POST">
 	<label for="name">Account Name
 	<input type="text" id="Name" name="Name" />
@@ -16,55 +16,64 @@ echo "Hello". $_SESSION["user"]["id"];?>
 	<input type="submit" name="Bank" value="Create Account"/>
 </form>
 <?php
-echo "before major if 1";
+//echo "before major if 1";
 require("common.inc.php");
 
-echo "before major if 2";
+//echo "before major if 2";
 if(isset($_POST["Bank"])){
-echo "before major if 2a";
+//echo "before major if 2a";
     $name = $_POST["Name"];
     
 	$Acctyp = $_POST["Account_Type"];
 	$balance = $_POST["Balance"];
-	echo "before major if 3";
+	//echo "before major if 3";
     if(!empty($name) && !empty($Acctyp)&& !empty($balance)){
-	    echo "before major if 3a<br>";
+	   // echo "before major if 3a<br>";
         require("config.php");
-	    echo "before major if 3b<br>";
+	  //  echo "before major if 3b<br>";
 	    
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-	    echo "Inside major if";
+	  //  echo "Inside major if";
         try{
-		echo "before major if 3c<br>";
+		//echo "before major if 3c<br>";
             $db = new PDO($connection_string, $dbuser, $dbpass);
-		echo "before major if 3d<br>";
+		//echo "before major if 3d<br>";
 		try{
 		$stmt1 = $db->prepare("SELECT id FROM Users where email = :email LIMIT 1");
-			echo "before major if 3e ".$email."<br>";
+			//echo "before major if 3e ".$email."<br>";
 		$stmt1->execute(array(
 					":email" => $email
 				));
 			$res = $stmt1->fetch(PDO::FETCH_ASSOC);
 		$user_id=$res["id"];
-			echo "AAAAAA ".$user_id."<br>";
+			//echo "AAAAAA ".$user_id."<br>";
 			//$user_id=1;
 		}catch (Exception $e1){
             echo $e1->getMessage();
 			//echo "setting<br>";
 			//$user_id=16;
         }
-            $stmt = $db->prepare("INSERT INTO Bank_Account (Name, Account_Type, Balance, User_id) VALUES (:name, :Acctyp,:balance,:user)");
+            /*$stmt = $db->prepare("INSERT INTO Bank_Account (Name, Account_Type, User_id) VALUES (:name, :Acctyp,:user)");
             $result = $stmt->execute(array(
                 ":name" => $name,
 				":Acctyp"=> $Acctyp,
-				":balance"=> $balance,
 		    ":user"=>$user_id
             ));
             $e = $stmt->errorInfo();
             if($e[0] != "00000"){
 		    echo "setting eee <br>";
                 echo var_export($e, true);
-            }
+            }*/
+		$stmt1 = $db->prepare("SELECT max(id) as id FROM Bank_Account where Name = :name and Account_Type=:Acctyp and User_id=:user");
+			//echo "before major if 3e ".$email."<br>";
+		$stmt1->execute(array(
+					":name" => $name,
+				":Acctyp"=> $Acctyp,
+		    ":user"=>$user_id
+				));
+			$res = $stmt1->fetch(PDO::FETCH_ASSOC);
+		$acc_id=$res["id"];
+		echo $acc_id;
             else{
                 //echo var_export($result, true);
                 if ($result){
