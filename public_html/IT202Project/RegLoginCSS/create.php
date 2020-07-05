64a85af99dce42d0addb11367f243452bd0809c2
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-05 19:28:26 -0400

    Update create.php

--- public_html/IT202Project/RegLoginCSS/create.php
+++ public_html/IT202Project/RegLoginCSS/create.php
@@ -16,42 +16,26 @@ echo "Hello". $email;?>
 	<input type="submit" name="Bank" value="Create Account"/>
 </form>
 <?php
-//echo "before major if 1";
 require("common.inc.php");
-
-//echo "before major if 2";
 if(isset($_POST["Bank"])){
-//echo "before major if 2a";
     $name = $_POST["Name"];
     
 	$Acctyp = $_POST["Account_Type"];
 	$balance = $_POST["Balance"];
-	//echo "before major if 3";
     if(!empty($name) && !empty($Acctyp)&& !empty($balance)){
-	   // echo "before major if 3a<br>";
         require("config.php");
-	  //  echo "before major if 3b<br>";
-	    
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-	  //  echo "Inside major if";
         try{
-		//echo "before major if 3c<br>";
             $db = new PDO($connection_string, $dbuser, $dbpass);
-		//echo "before major if 3d<br>";
 		try{
 		$stmt1 = $db->prepare("SELECT id FROM Users where email = :email LIMIT 1");
-			//echo "before major if 3e ".$email."<br>";
 		$stmt1->execute(array(
 					":email" => $email
 				));
 			$res = $stmt1->fetch(PDO::FETCH_ASSOC);
 		$user_id=$res["id"];
-			//echo "AAAAAA ".$user_id."<br>";
-			//$user_id=1;
 		}catch (Exception $e1){
             echo $e1->getMessage();
-			//echo "setting<br>";
-			//$user_id=16;
         }
             $stmt = $db->prepare("INSERT INTO Bank_Account (Name, Account_Type, User_id) VALUES (:name, :Acctyp,:user)");
             $result = $stmt->execute(array(
@@ -65,7 +49,6 @@ if(isset($_POST["Bank"])){
                 echo var_export($e, true);
             }
 		$stmt1 = $db->prepare("SELECT max(id) as id FROM Bank_Account where Name = :name and Account_Type=:Acctyp and User_id=:user");
-			//echo "before major if 3e ".$email."<br>";
 		$stmt1->execute(array(
 					":name" => $name,
 				":Acctyp"=> $Acctyp,
@@ -93,7 +76,6 @@ if(isset($_POST["Bank"])){
             if($e[0] != "00000"){
 		    var_dump($e);
 		    echo "setting eee ".$e."<br>";
-                //echo var_export($e, true);
             }
 		$balance =$balance * -1;
 		echo $balance;
@@ -111,29 +93,18 @@ if(isset($_POST["Bank"])){
 		    var_dump($e);
 		    $stmt2->debugDumpParams();
 		    echo "setting AAAAAeee ".$e."<br>";
-                //echo var_export($e, true);
             }
 		$stmt = $db->prepare("update Bank_Account set Balance= (SELECT sum(Amount) FROM Transactions WHERE Acc_Src=:accnum) where Account_Number=:accnum");
             $result = $stmt->execute(array(
                 ":accnum" => $account_num
             ));
-            //$e = $stmt->errorInfo();
-            //if($e[0] != "00000"){
-		  //  echo "setting eee <br>";
-                //echo var_export($e, true);
-           // }
-		//echo $acc_id;
-            //else{
-                //echo var_export($result, true);
                 if ($result){
-		//if($res){
                     echo "Successfully inserted new thing: " . $name;
                 }
                 else{
                     echo "Error inserting record";
                 }
             }
-        //}
         catch (Exception $e){
 		echo "Error inserting record 1";
             echo $e->getMessage();
