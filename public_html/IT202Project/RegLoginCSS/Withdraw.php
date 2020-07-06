1fb34a9d33868156ca80d2f3ea6a5bbb22292d6e
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-05 20:44:56 -0400

    Update Withdraw.php

--- public_html/IT202Project/RegLoginCSS/Withdraw.php
+++ public_html/IT202Project/RegLoginCSS/Withdraw.php
@@ -49,7 +49,6 @@ if(isset($_POST["Withdraw"])){
             }
 		$balance =$balance * -1;
 		echo $balance;
-		
 		$stmt2 = $db->prepare("INSERT INTO Transactions (Acc_Src, Acc_Dst,Type,Amount,Expected_total) VALUES (:acc1,:acc, :typ,:balance,:exp_balance)");
             $result1 = $stmt2->execute(array(
 		    ":acc1" => "000000000000",
