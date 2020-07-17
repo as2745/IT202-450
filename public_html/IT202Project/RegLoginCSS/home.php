f6095b76d702427b4b617c09e59d74e433b8921e
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-17 18:38:35 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -10,10 +10,4 @@ require("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $stmt = $db->prepare("SELECT * FROM Bank_Account");
 $stmt->execute();
-echo $stmt["Acc_Src"];
-echo $stmt["Acc_Dst"];
-echo $stmt["Type"];
-echo $stmt["Expected_Total"];
-echo $stmt["Created"];
-echo $stmt["Amount"];
 ?>
