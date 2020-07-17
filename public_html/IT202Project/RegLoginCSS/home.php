60fa44860565307d82e4b413b4e7c8504d02df64
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-17 19:29:58 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -5,6 +5,8 @@ include("header.php");
 <?php 
 echo "Welcome, " . $_SESSION["user"]["email"];
 echo var_export($_SESSION, true);
+?>
+<?php
 require("common.inc.php");
 require("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
