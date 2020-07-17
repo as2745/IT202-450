7ea99b26000d80886c339b5958a9413eba9be787
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-17 19:23:16 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -9,6 +9,6 @@ require("common.inc.php");
 require("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $stmt = $db->prepare("SELECT * FROM Bank_Account");
-$result=$stmt->execute();
-var_dump($result);
+//$result=$stmt->execute();
+var_dump($stmt);
 ?>
