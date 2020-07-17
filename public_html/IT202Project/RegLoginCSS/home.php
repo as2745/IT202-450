91aec80e62d9ddf3fdf1132a31c28a33f8cef9d3
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-17 17:32:45 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -1,11 +1,12 @@
 <?php
 include("header.php");
-
 ?>
 <h4>Home</h4>
-<?
+<?php 
+echo "Welcome, " . $_SESSION["user"]["email"];
+echo var_export($_SESSION, true);
 require("common.inc.php");
-        require("config.php");
+require("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $stmt = $db->prepare("SELECT * FROM Bank_Account");
 $stmt->execute();
