f124384a2e9dac02ca1b54a080c4e5ecf1297c88
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 09:44:51 -0400

    Update AcctDetails.php

--- public_html/IT202Project/RegLoginCSS/AcctDetails.php
+++ public_html/IT202Project/RegLoginCSS/AcctDetails.php
@@ -6,14 +6,8 @@ require("config.php");
 $email=$_SESSION["user"]["email"];
 $account=$_GET["account"];
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-var_dump($connection_string);
-echo "Details of ".$account."<br>";
-var_dump($dbuser);
-echo "Details of ".$account."<br>";
-var_dump($dbpass);
-echo "Details of ".$account."<br>";
 $db = new PDO($connection_string, $dbuser, $dbpass);
-$stmt = $db->prepare("SELECT * FROM Transactions where Acc_Dst=:acc");
+$stmt = $db->prepare("SELECT count(*) FROM Transactions where Acc_Dst=:acc");
 $stmt->execute(array(
 	":acc" => $account
 ));
