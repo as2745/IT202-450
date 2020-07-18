b611cf1babc0a3128df42b440631cb8546c68be9
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 09:49:07 -0400

    Update AcctDetails.php

--- public_html/IT202Project/RegLoginCSS/AcctDetails.php
+++ public_html/IT202Project/RegLoginCSS/AcctDetails.php
@@ -7,7 +7,7 @@ $email=$_SESSION["user"]["email"];
 $account=$_GET["account"];
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $db = new PDO($connection_string, $dbuser, $dbpass);
-$stmt = $db->prepare("SELECT count(*) FROM Transactions where Acc_Dst=:acc");
+$stmt = $db->prepare("SELECT count(*) as num FROM Transactions where Acc_Dst=:acc");
 $stmt->execute(array(
 	":acc" => $account
 ));
@@ -18,5 +18,16 @@ if($e[0] != "00000"){
 	echo "setting eee ".$e."<br>";
 }
 var_dump($res);
+$stmt1 = $db->prepare("SELECT * FROM Bank_Account where Account_Number=:acc");
+$stmt1->execute(array(
+	":acc" => $account
+));
+$result = $stmt1->fetchAll();
+$e = $stmt1->errorInfo();
+if($e[0] != "00000"){
+	var_dump($e);
+	echo "setting eee ".$e."<br>";
+}
+var_dump($result);
 echo "Details of ".$account."<br>";
 echo "Hello". $email;?>
