d3909741582953d6b4d73c65fa9a903e8da19a73
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 10:07:39 -0400

    Update AcctDetails.php

--- public_html/IT202Project/RegLoginCSS/AcctDetails.php
+++ public_html/IT202Project/RegLoginCSS/AcctDetails.php
@@ -32,7 +32,7 @@ if($e[0] != "00000"){
 $type=$result[0]["Account_Type"];
 $amount=$result[0]["Balance"];
 
-echo "<h3>Details of ".$account."</h3><br>";
-echo "<h4>Account Type: ".$type."</h4><br>";
-echo "<h4>Balance : ".$amount."</h4><br>";
+echo "<h3>Details of ".$account."</h3>";
+echo "<h4>Account Type: ".$type."</h4>";
+echo "<h4>Balance : ".$amount."</h4>";
 ?>
