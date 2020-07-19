7f08de6639e56596d9d4d4063c359d9c4c122569
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 21:18:41 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -3,9 +3,8 @@ include("header.php");
 require("common.inc.php");
 require("config.php");
 $accounts=$_SESSION["user"]["accounts"];
-echo "after accounts";
 $new_arr = array_column($accounts,'Account_Number');
         foreach($new_arr as $item){
-        var_dump($item);
+                echo $item;
         }
 ?>
