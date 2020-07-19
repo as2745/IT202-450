56526fa1f4ecf8b794918e8ae0e33a19f5a0095c
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 20:50:55 -0400

    Update pages.php

--- public_html/IT202Project/RegLoginCSS/pages.php
+++ public_html/IT202Project/RegLoginCSS/pages.php
@@ -64,7 +64,7 @@ $results = $stmt->fetchAll();
 </table>
 
 <div style="height: 30px;"></div>
-<table width="50%" align="left" border="1px solid black">
+<table width="50%" align="left" >
     <tr>
 
         <td valign="top" align="left"></td>
