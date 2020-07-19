52141dd265e47981b282ae6f809b899c8b4a8f0a
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 22:25:18 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -6,7 +6,7 @@ $accounts=$_SESSION["user"]["accounts"];
 $new_arr = array_column($accounts,'Account_Number');
         foreach($new_arr as $item){
                 echo $item;
-                 echo '<a href="Deposit.php">Deposit</a>'; 
+                 echo '<a href="Deposit.php?Account_Number=000000000023">Deposit</a>'; 
                  echo '<a href="Withdraw.php">Withdraw</a>';
                  echo '<a href="Transfer.php">Transfer</a>';
                 echo '<br>';
