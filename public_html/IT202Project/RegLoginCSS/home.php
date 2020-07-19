e2fe75014f202f20a71fe8ac65ac1292224dd5c4
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 21:13:35 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -1,15 +1,20 @@
 <?php
 include("header.php");
+require("common.inc.php");
+require("config.php");
 $accounts=$_SESSION["user"]["accounts"];
+echo "after accounts";
 $new_arr = array_column($accounts,'Account_Number');
+echo "after new_arr";
 //<h4>Home</h4>
 //echo "Welcome, " . $_SESSION["user"]["email"];
 //echo var_export($_SESSION, true);
-require("common.inc.php");
-require("config.php");
+
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $stmt = $db->prepare("SELECT Account_Number FROM Bank_Account");
+echo "after query";
 $stmt->execute();
+echo "after execution";
         foreach($new_arr as $item){
         var_dump($item);
         }
