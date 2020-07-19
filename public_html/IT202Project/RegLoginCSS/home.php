4aff9c47b80f1721c939577bbfa4649daaa884da
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 21:06:40 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -2,13 +2,9 @@
 include("header.php");
 $accounts=$_SESSION["user"]["accounts"];
 $new_arr = array_column($accounts,'Account_Number');
-?>
 <h4>Home</h4>
-<?php 
 echo "Welcome, " . $_SESSION["user"]["email"];
 //echo var_export($_SESSION, true);
-?>
-<?php
 require("common.inc.php");
 require("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
