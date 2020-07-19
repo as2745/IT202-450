de3927ef4c9cc99ae64a56855f6108cdcc151c11
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 22:39:39 -0400

    Update AcctDetails.php

--- public_html/IT202Project/RegLoginCSS/AcctDetails.php
+++ public_html/IT202Project/RegLoginCSS/AcctDetails.php
@@ -36,6 +36,8 @@ echo "<h3>Details of ".$account."</h3>";
 echo "<h4>Account Type: ".$type."</h4>";
 echo "<h4>Balance : $".$amount."</h4>";
 ?>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <input type="text" placeholder="From Date" id="post_at" name="post_at"   />
