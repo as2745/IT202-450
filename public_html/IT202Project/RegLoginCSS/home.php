164c241b574b91f4bc0314098d92399aeba86041
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-18 21:05:19 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -6,7 +6,7 @@ $new_arr = array_column($accounts,'Account_Number');
 <h4>Home</h4>
 <?php 
 echo "Welcome, " . $_SESSION["user"]["email"];
-echo var_export($_SESSION, true);
+//echo var_export($_SESSION, true);
 ?>
 <?php
 require("common.inc.php");
