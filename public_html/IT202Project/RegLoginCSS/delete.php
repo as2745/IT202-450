efafa85ce9eeeb0ac0cb3ebeab43078e27c55d9a
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-22 20:52:51 -0400

    Update delete.php

--- public_html/IT202Project/RegLoginCSS/delete.php
+++ public_html/IT202Project/RegLoginCSS/delete.php
@@ -1,16 +1,16 @@
 <form method="POST">
-	
 	<label for="accnumber">Account Number
 	<input type="number" id="AccNum" name="Account_Number" />
 	</label>
-	
 	<input type="submit" name="Bank" value="Delete Account"/>
 </form>
 <?php
 require("common.inc.php");
 $db = getDB();
+$Accnum1 = $_POST["Account_Number"];
 //example usage, change/move as needed
-$stmt = $db->prepare("UPDATE Bank_Accounts SET Status=Inactive WHERE Id=1");
-			$result = $stmt->execute
-			var_dump($result);
+$stmt = $db->prepare("DELETE from Bank_Account where Account_Number=$Accnum1");
+                    $result = $stmt->execute(array(
+                        ":id" => $Accnum1
+                    ));
 ?>
