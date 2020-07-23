fbe09ce8ed7223c3d9f08e1bf6781ff8821759ea
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-22 21:31:39 -0400

    Update delete.php

--- public_html/IT202Project/RegLoginCSS/delete.php
+++ public_html/IT202Project/RegLoginCSS/delete.php
@@ -12,7 +12,10 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $db = new PDO($connection_string, $dbuser, $dbpass);
 $Accnum1 = $_POST["Account_Number"];
 //example usage, change/move as needed
-$stmt = $db->prepare("UPDATE Bank_Accounts SET Status = Inactive WHERE Account_Number='$Accnum1'");
+$stmt = $db->prepare("UPDATE Bank_Accounts SET Status = Inactive WHERE Account_Number==:acc");
+$stmt->execute(array(
+	":acc" => $Accnum1
+));
 	var_dump($stmt);
-$result = $stmt->execute();
+//$result = $stmt->execute();
 ?>
