4b3468bdfd663d19f7b94c58fa96f90bad838765
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-22 21:03:32 -0400

    Update delete.php

--- public_html/IT202Project/RegLoginCSS/delete.php
+++ public_html/IT202Project/RegLoginCSS/delete.php
@@ -6,11 +6,13 @@
 </form>
 <?php
 require("common.inc.php");
+var_dump($Accnum1);
 $db = getDB();
+var_dump($stmt);
 $Accnum1 = $_POST["Account_Number"];
 //example usage, change/move as needed
 $stmt = $db->prepare("UPDATE Bank_Accounts SET Status = Inactive WHERE Account_Number=$Accnum1");
                     $result = $stmt->execute();
 var_dump($Accnum1);
-var_dump($stmt);
+
 ?>
