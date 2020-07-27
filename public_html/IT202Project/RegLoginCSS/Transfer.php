123cccbbb968ca5366f1393036ae781ff472d73c
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-26 21:55:56 -0400

    Update Transfer.php

--- public_html/IT202Project/RegLoginCSS/Transfer.php
+++ public_html/IT202Project/RegLoginCSS/Transfer.php
@@ -32,22 +32,16 @@ echo "Hello". $email;?>
 	<input type="submit" name="Transfer" value="Transfer"/>
 </form>
 <?php
-//echo "before major if 1";
 require("common.inc.php");
 require("config.php");
-//echo "before major if 2";
 if(isset($_POST["Transfer"])){
-//echo "before major if 2a";
     $name = $_POST["Name"];
     $name1 = $_POST["Name1"];
 	
 	$balance = $_POST["Balance"];
-  //$balance=$balance * -1;
-	//echo "before major if 3".$name;
-	//echo "before major if 3".$name1;
 	$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 	$db = new PDO($connection_string, $dbuser, $dbpass);
-	$stmt1 = $db->prepare("SELECT * FROM Bank_Account where Account_Number=:acc");
+	$stmt1 = $db->prepare("SELECT * FROM Bank_Accounts where Account_Number=:acc");
 	$stmt1->execute(array(
 		":acc" => $name
 	));
@@ -62,18 +56,10 @@ if(isset($_POST["Transfer"])){
 	$amount1=$amount1+$balance;
 	
     if(!empty($name) && !empty($balance) && $balance>0 &&  $amount>5){
-	   // echo "before major if 3a<br>";
-        
-	  //  echo "before major if 3b<br>";
-	    
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-	  //  echo "Inside major if";
         try{
-		//echo "before major if 3c<br>";
             $db = new PDO($connection_string, $dbuser, $dbpass);
-		//echo "before major if 3d<br>";
-		$balance=$balance * -1;
-		
+		$balance=$balance * -1;		
 		$stmt = $db->prepare("INSERT INTO Transactions (Acc_Src, Acc_Dst,Type,Amount,Expected_total) VALUES (:accnum,:accnum1, :typ,:balance,:exp_balance)");
             $result = $stmt->execute(array(
 		    ":accnum" => $name,
@@ -86,11 +72,8 @@ if(isset($_POST["Transfer"])){
             if($e[0] != "00000"){
 		    var_dump($e);
 		    echo "setting eee ".$e."<br>";
-                //echo var_export($e, true);
             }
 		$balance =$balance * -1;
-		//echo $balance;
-		
 		$stmt2 = $db->prepare("INSERT INTO Transactions (Acc_Src, Acc_Dst,Type,Amount,Expected_total) VALUES (:acc1,:acc, :typ,:balance,:exp_balance)");
             $result1 = $stmt2->execute(array(
 		    ":acc1" => $name1,
@@ -104,26 +87,15 @@ if(isset($_POST["Transfer"])){
 		    var_dump($e);
 		    $stmt2->debugDumpParams();
 		    echo "setting AAAAAeee ".$e."<br>";
-                //echo var_export($e, true);
             }
-		$stmt = $db->prepare("update Bank_Account set Balance= (SELECT sum(Amount) FROM Transactions WHERE Acc_Src=:accnum) where Account_Number=:accnum");
+		$stmt = $db->prepare("update Bank_Accounts set Balance= (SELECT sum(Amount) FROM Transactions WHERE Acc_Src=:accnum) where Account_Number=:accnum");
             $result = $stmt->execute(array(
                 ":accnum" => $name
             ));
-          //$stmt = $db->prepare("update Bank_Account set Balance= (SELECT sum(Amount) FROM Transactions WHERE Acc_Dst=:accnum) where Account_Number=:accnum");
             $res = $stmt->execute(array(
                 ":accnum" => $name1
             ));
-            //$e = $stmt->errorInfo();
-            //if($e[0] != "00000"){
-		  //  echo "setting eee <br>";
-                //echo var_export($e, true);
-           // }
-		//echo $acc_id;
-            //else{
-                //echo var_export($result, true);
                 if ($result){
-		//if($res){
                     echo "Successfully transferred ".$balance." from account " . $name." to account ".$name1;
 			header("Location: home.php");
                 }
@@ -131,7 +103,6 @@ if(isset($_POST["Transfer"])){
                     echo "Error inserting record";
                 }
             }
-        //}
         catch (Exception $e){
 		echo "Error inserting record 1";
             echo $e->getMessage();
@@ -139,10 +110,9 @@ if(isset($_POST["Transfer"])){
     }
 	
     else{
-	   // echo "did not go through if";
         echo "Account and Amount must not be empty. Amount has to be greater than zero. As a result of transfer accounts should maintain 5 Dollar balance.";
     }
 }
-$stmt = $db->prepare("SELECT * FROM Bank_Account");
+$stmt = $db->prepare("SELECT * FROM Bank_Accounts");
 $stmt->execute();
 ?>
