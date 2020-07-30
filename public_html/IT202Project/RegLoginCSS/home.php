ed8ae3e87695c8bd0c71e03ab00c5c3bcd5427ac
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-30 19:08:49 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -4,6 +4,7 @@ require("common.inc.php");
 require("config.php");
 $accounts=$_SESSION["user"]["accounts"];
 $new_arr = array_column($accounts,'Account_Number');
+echo "<a href=create.php>Create a new account</a>"; 
         foreach($new_arr as $item){
                 echo "<a href=AcctDetails.php?account=". $item.">".$item."</a>"; 
                  echo "<a href=Deposit.php?Account_Number=". $item.">Deposit</a>"; 
