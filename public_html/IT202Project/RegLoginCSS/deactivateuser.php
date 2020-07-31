dc4341dbbb8812fbb73078c37249be6095f551a9
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-30 21:18:18 -0400

    Update deactivateuser.php

--- public_html/IT202Project/RegLoginCSS/deactivateuser.php
+++ public_html/IT202Project/RegLoginCSS/deactivateuser.php
@@ -1,20 +1,14 @@
-<form method="POST">
-	<label for="accnumber">Account Number
-	<input type="number" id="AccNum" name="Account_Number" />
-	</label>
-	<input type="submit" name="Bank" value="Deactivate Account"/>
-</form>
 <?php
+
 include("header.php");
 require("common.inc.php");
 require("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $db = new PDO($connection_string, $dbuser, $dbpass);
-$Accnum1 = $_POST["Account_Number"];
+$name = $_GET['email'];
 //example usage, change/move as needed
-$stmt = $db->prepare("UPDATE Bank_Accounts SET Status = 'Inactive' WHERE Account_Number=:acc");
+$stmt = $db->prepare("UPDATE User SET Active = 'No' WHERE email=:acc");
 $stmt->execute(array(
-	":acc" => $Accnum1
+	":acc" => $name
 ));
-	var_dump($stmt);
 ?>
