1325a0eb2c3e1e079603e0f38d1b6c3c8b121642
as2745 <60445218+as2745@users.noreply.github.com>
2020-07-30 20:58:50 -0400

    Update home.php

--- public_html/IT202Project/RegLoginCSS/home.php
+++ public_html/IT202Project/RegLoginCSS/home.php
@@ -2,7 +2,26 @@
 include("header.php");
 require("common.inc.php");
 require("config.php");
-$accounts=$_SESSION["user"]["accounts"];
+
+
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+$db = new PDO($connection_string, $dbuser, $dbpass);
+
+$email=$_GET["email"];
+$pwdreset=$_GET["resetpassword"];
+if(!empty($pwdreset)){
+}
+if(empty($email)){
+  $accounts=$_SESSION["user"]["accounts"];
+}
+else{
+  $stmt = $db->prepare("SELECT First_name, Last_name, Id from User where email='$email'");
+  $stmt->execute();
+  $result = $stmt->fetchAll();
+  $fname=$result[0]["First_name"];
+  $lname=$result[0]["Last_name"];
+  $id=$result[0]["Id"];
+}
 
 $new_arr = array_column($accounts,'Account_Number');
 echo "<a href=create.php>Create a new account</a>"; 
