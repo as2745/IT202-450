6560db7c1d8f6896a933b364d4b987714ac617aa
as2745 <60445218+as2745@users.noreply.github.com>
2020-08-02 18:19:21 -0400

    Update delete.php

--- public_html/IT202Project/RegLoginCSS/delete.php
+++ public_html/IT202Project/RegLoginCSS/delete.php
@@ -1,20 +1,30 @@
-<form method="POST">
-	<label for="accnumber">Account Number
-	<input type="number" id="AccNum" name="Account_Number" />
-	</label>
-	<input type="submit" name="Bank" value="Deactivate Account"/>
-</form>
 <?php
 include("header.php");
 require("common.inc.php");
 require("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $db = new PDO($connection_string, $dbuser, $dbpass);
-$Accnum1 = $_POST["Account_Number"];
+$Accnum1 = $_GET["Account_Number"];
 //example usage, change/move as needed
-$stmt = $db->prepare("UPDATE Bank_Accounts SET Status = 'Inactive' WHERE Account_Number=:acc and Balance=0.00");
+$stmt = $db->prepare("SELECT * from Bank_Accounts WHERE Account_Number=:acc");
 $stmt->execute(array(
 	":acc" => $Accnum1
 ));
-	var_dump($stmt);
+$result=$stmt->fetchall();
+if($result[0]["Account_Type"]=="Loan"){
+	$stmt1 = $db->prepare("SELECT * from Link_Account WHERE Account_Number=:acc");
+	$stmt1->execute(array(
+		":acc" => $Accnum1
+	));
+	$res=$stmt1->fetchall();
+	$bal=$res[0]["Balance"];
+}
+else $bal=$result[0]["Balance"];
+if($bal<=0){
+$stmt = $db->prepare("UPDATE Bank_Accounts SET Status = 'Inactive' WHERE Account_Number=:acc");
+$stmt->execute(array(
+	":acc" => $Accnum1
+));
+} else echo 'Account Balance should be 0 before deactivating';
+	
 ?>
