cd80d3c1e30c44c30d6344d801c986050e68d058
as2745 <60445218+as2745@users.noreply.github.com>
2020-08-01 23:20:10 -0400

    Update create.php

--- public_html/IT202Project/RegLoginCSS/create.php
+++ public_html/IT202Project/RegLoginCSS/create.php
@@ -4,8 +4,9 @@ include("header.php");
 $email=$_SESSION["user"]["email"];
 $accounts=$_SESSION["user"]["accounts"];
 $new_arr = array_column($accounts,'Account_Number');
-echo "Hello". $email;?>
-echo "<a href=createloan.php>Create a Loan</a>"; 
+echo "Hello". $email;
+echo "<br>";
+echo "<a href=createloan.php>Create a Loan</a>"; ?>
 <form method="POST">
 	<label for="name">Account Name
 	<input type="text" id="Name" name="Name" />
