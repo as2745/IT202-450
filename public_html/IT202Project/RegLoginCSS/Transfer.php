9ea59444a0f1e1548535095d28411df81a310e4d
as2745 <60445218+as2745@users.noreply.github.com>
2020-08-03 16:38:37 -0400

    Update Transfer.php

--- public_html/IT202Project/RegLoginCSS/Transfer.php
+++ public_html/IT202Project/RegLoginCSS/Transfer.php
@@ -1,10 +1,10 @@
 <?php
 include("header.php");
-
 $email=$_SESSION["user"]["email"];
 $accounts=$_SESSION["user"]["accounts"];
 $new_arr = array_column($accounts,'Account_Number');
 $account=$_GET["Account_Number"];
+//var_dump($new_arr);
 echo "Hello". $email;
 echo "<br>";
 echo "<a href=IntraTransfer.php?Account_Number=".$account.">Transfer to different User</a>"; 
@@ -17,6 +17,10 @@ echo "<br>";?>
 	</label>
 	<select name="Name1" id="Name1">
 		<?php
+        // A sample product array
+        //$products = array("Mobile", "Laptop", "Tablet", "Camera");
+        
+        // Iterating through the product array
         foreach($new_arr as $item){
         ?>
         <option value="<?php echo strtolower($item); ?>"><?php echo $item; ?></option>
@@ -52,13 +56,13 @@ if(isset($_POST["Transfer"])){
 	$result = $stmt1->fetchAll();
 	$amount1=$result[0]["Balance"];
 	$amount1=$amount1+$balance;
-	
-    if(!empty($name) && !empty($balance) && $balance>0 && $amount>0){
+
+    if(!empty($name) && !empty($balance) && $balance>0 &&  $amount>0){
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
 		$balance=$balance * -1;		
-		$stmt = $db->prepare("INSERT INTO Transactions (Acc_Src, Acc_Dst,Transaction_Type,Amount,Expected_total) VALUES (:accnum,:accnum1, :typ,:balance,:exp_balance)");
+		$stmt = $db->prepare("INSERT INTO Transactions (Acc_Src, Acc_Dst,Type,Amount,Expected_total) VALUES (:accnum,:accnum1, :typ,:balance,:exp_balance)");
             $result = $stmt->execute(array(
 		    ":accnum" => $name,
 		    ":accnum1" => $name1,
@@ -68,10 +72,11 @@ if(isset($_POST["Transfer"])){
             ));
 		$e = $stmt->errorInfo();
             if($e[0] != "00000"){
-		    echo var_export($e, true);
+		    var_dump($e);
+		    echo "setting eee ".$e."<br>";
             }
 		$balance =$balance * -1;
-		$stmt2 = $db->prepare("INSERT INTO Transactions (Acc_Src, Acc_Dst,Transaction_Type,Amount,Expected_total) VALUES (:acc1,:acc, :typ,:balance,:exp_balance)");
+		$stmt2 = $db->prepare("INSERT INTO Transactions (Acc_Src, Acc_Dst,Type,Amount,Expected_total) VALUES (:acc1,:acc, :typ,:balance,:exp_balance)");
             $result1 = $stmt2->execute(array(
 		    ":acc1" => $name1,
 		    ":acc" => $name,
@@ -81,7 +86,9 @@ if(isset($_POST["Transfer"])){
             ));
 		$e = $stmt2->errorInfo();
             if($e[0] != "00000"){
-		    echo var_export($e, true);
+		    var_dump($e);
+		    $stmt2->debugDumpParams();
+		    echo "setting AAAAAeee ".$e."<br>";
             }
 		$stmt = $db->prepare("update Bank_Accounts set Balance= (SELECT sum(Amount) FROM Transactions WHERE Acc_Src=:accnum) where Account_Number=:accnum");
             $result = $stmt->execute(array(
@@ -99,12 +106,13 @@ if(isset($_POST["Transfer"])){
                 }
             }
         catch (Exception $e){
+		echo "Error inserting record 1";
             echo $e->getMessage();
         }
     }
 	
     else{
-        echo "Account and Amount must not be empty. Transfer Amount has to be greater than zero.";
+        echo "Account and Amount must not be empty. Amount has to be greater than zero. As a result of transfer accounts should maintain 5 Dollar balance.";
     }
 }
 $stmt = $db->prepare("SELECT * FROM Bank_Accounts");
